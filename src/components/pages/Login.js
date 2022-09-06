ad651193fc165566b51b132ab53ca8132fc7eb92
Manoj <mdas53096@gmail.com>
2022-09-06 15:44:05 +0530

    Login with auth service

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -9,10 +9,13 @@ import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { useHistory } from "react-router-dom";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { withRouter } from "react-router";
+import { createHashHistory } from 'history'
+import { environment } from '../../environments/environment';
 import Diamond from '../../assest/collections_login.png';
 import LoginRight from '../../assest/qwerty.svg';
 
@@ -33,44 +36,12 @@ function Copyright(props) {
 }
 
 const theme = createTheme();
+export const history = createHashHistory();
 
-// const useStyles = makeStyles(theme => ({
-//     root: {
-//         display: 'flex',
-//         flexDirection: 'column',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         padding: theme.spacing(2),
-
-//         '& .MuiTextField-root': {
-//             margin: theme.spacing(1),
-//             width: '300px',
-//         },
-//         '& .MuiButtonBase-root': {
-//             margin: theme.spacing(2),
-//         },
-//     },
-// }));
-const LogIn = () => {
-
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const data = new FormData(event.currentTarget);
-    //     console.log({
-    //         email: data.get('email'),
-    //         password: data.get('password'),
-    //     });
-    // const classes = useStyles();
+export default function LogIn(props) {
     const { useState } = React;
     const [APIData, setAPIData] = useState([]);
-
-    useEffect(() => {
-        axios.get(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`)
-            .then((response) => {
-                console.log(response.data)
-                setAPIData(response.data);
-            })
-    }, []);
+    const history = useHistory();
 
     const [eye, seteye] = useState(true);
     const [inpass, setinpass] = useState("password");
@@ -109,7 +80,6 @@ const LogIn = () => {
         }
     }
 
-
     const inputEvent = (event) => {
 
         const name = event.target.name;
@@ -147,95 +117,112 @@ const LogIn = () => {
         }
     });
 
+    const handleSubmit = (event) => {
+        const data = new FormData(event.currentTarget);
+        console.log(data);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+        event.preventDefault();
+        axios.post(`${environment.apiUrl}/branchManager/login`, { email: data.get('email'),
+                    password: data.get('password')
+                }
+                // { withCredentials: true }
+            )
+            .then(res => {
+                console.log(res);
+                window.localStorage.setItem("token", res.data.token);
+                history.push("/home");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
     return (
-        <>
-            <ThemeProvider theme={theme}>
-                <Grid container component="main" sx={{ height: '100vh' }}>
-                    <CssBaseline />
-                    <Grid
-                        item
-                        xs={false}
-                        sm={5}
-                        md={8}
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={5}
+                    md={8}
+                    sx={{
+                        backgroundImage: 'url(' + Diamond + ')',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={4}
+                    component={Paper}
+                    elevation={6} square
+                    sx={{
+                        backgroundImage: 'url(' + LoginRight + ')',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: '100%',
+                        backgroundPosition: 'center',
+                    }}
+                >
+                    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                    <Box
                         sx={{
-                            backgroundImage: 'url(' + Diamond + ')',
-                            backgroundRepeat: 'no-repeat',
-                            backgroundColor: (t) =>
-                                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                        }}
-                    />
-                    <Grid
-                        item
-                        xs={12}
-                        sm={8}
-                        md={4}
-                        component={Paper}
-                        elevation={6} square
-                        sx={{
-                            backgroundImage: 'url(' + LoginRight + ')',
-                            backgroundRepeat: 'no-repeat',
-                            backgroundColor: (t) =>
-                                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                            backgroundSize: '100%',
-                            backgroundPosition: 'center',
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
                         }}
                     >
-                        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-                        <Box
-                            sx={{
-                                my: 8,
-                                mx: 4,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <Box component="form" noValidate sx={{ mt: 1 }}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    autoFocus
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                />
-                                {/* <FormControlLabel
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            {/* <FormControlLabel
                                     control={<Checkbox value="remember" color="primary" />}
                                     label="Remember me"
                                 /> */}
-                                <ThemeProvider theme={customTheme}>
-                                    <Button
-                                        type="submit"
-                                        fullWidth
-                                        variant="contained"
-                                        sx={{ mt: 3, mb: 2 }}
-                                    >
-                                        Sign In
-                                    </Button>
-                                </ThemeProvider>
-                            </Box>
+                            <ThemeProvider theme={customTheme}>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Sign In
+                                </Button>
+                            </ThemeProvider>
                         </Box>
-                    </Grid>
+                    </Box>
                 </Grid>
-            </ThemeProvider>
-
-
-        </>
+            </Grid>
+        </ThemeProvider>
     );
 }
-
-export default LogIn;
