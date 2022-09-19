4870bc40388fc45b9fa5b69246f1c34e7db05f09
easwarmanish <manishe@newstreettech.com>
2022-09-19 14:47:14 +0530

    Latest code

--- src/components/foTable/foTable.jsx
+++ src/components/foTable/foTable.jsx
@@ -8,6 +8,11 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Grid } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 // import SearchBar from "material-ui-search-bar";
 
 import AttendanceBox from '../attendance/attendance';
@@ -33,6 +38,7 @@ const rows = [
 ];
 
 export default function BasicTable() {
+  const [value, setValue] = React.useState ();
   const [selectedRow, setSelectedRow] = React.useState({});
 
   const history = useHistory();
@@ -44,52 +50,63 @@ export default function BasicTable() {
   return (
     <Grid container spacing={3}>
       <Grid item xs={2}>
-      <div><Calendar /></div>
-      <br />
-      <div><AttendanceBox /></div>
-      <br />
-      <div><CollectionWidget /></div>
-      <br />
-      <div><CashDetails /></div>
+        <div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Today"
+              value={value}
+              onChange={(newValue) => {
+                setValue(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+        </div>
+        <br />
+        <div><AttendanceBox /></div>
+        <br />
+        <div><CollectionWidget /></div>
+        <br />
+        <div><CashDetails /></div>
       </Grid>
       <Grid item xs={10}>
-      <TableContainer component={Paper}>
-      <Table sx={{ minwidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>NAME</TableCell>
-            <TableCell align="center"># OF CUSTOMERS</TableCell>
-            <TableCell align="center">COLLECTED</TableCell>
-            <TableCell align="center">DPD</TableCell>
-            <TableCell align="center">RESCHEDULED</TableCell>
-            <TableCell align="center">TARGET</TableCell>
-            <TableCell align="center">PENDING</TableCell>
-            <TableCell align="center">GIVEN AT BRANCH</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              hover = {true}
-              key = {row.name}
-              sx = {{ '&:last-child td, &:last-child th': { border: 0 } }}
-              onClick = {groupPage}
-            >
-              <TableCell component="th" scope="row">
-                <b>{row.name}</b>
-              </TableCell>
-              <TableCell align="center">{row.noOfCustomers}</TableCell>
-              <TableCell align="center">{row.collected}</TableCell>
-              <TableCell align="center">{row.dpd}</TableCell>
-              <TableCell align="center">{row.rescheduled}</TableCell>
-              <TableCell align="center">{row.target}</TableCell>
-              <TableCell align="center">{row.pending}</TableCell>
-              <TableCell align="center">{row.givenAtBranch}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        <TableContainer component={Paper}>
+          <Table sx={{ minwidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>NAME</TableCell>
+                <TableCell align="center"># OF CUSTOMERS</TableCell>
+                <TableCell align="center">COLLECTED</TableCell>
+                <TableCell align="center">DPD</TableCell>
+                <TableCell align="center">RESCHEDULED</TableCell>
+                <TableCell align="center">TARGET</TableCell>
+                <TableCell align="center">PENDING</TableCell>
+                <TableCell align="center">GIVEN AT BRANCH</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  hover={true}
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  onClick={groupPage}
+                >
+                  <TableCell component="th" scope="row">
+                    <b>{row.name}</b>
+                  </TableCell>
+                  <TableCell align="center">{row.noOfCustomers}</TableCell>
+                  <TableCell align="center">{row.collected}</TableCell>
+                  <TableCell align="center">{row.dpd}</TableCell>
+                  <TableCell align="center">{row.rescheduled}</TableCell>
+                  <TableCell align="center">{row.target}</TableCell>
+                  <TableCell align="center">{row.pending}</TableCell>
+                  <TableCell align="center">{row.givenAtBranch}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   );
